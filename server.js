f1e24f1d09535238d389295cd45709d34b8c0adf
dobashishishi <dobasho0418@gmail.com>
2025-06-19 12:37:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 5173;
+const PORT = process.env.PORT || 5173;
 
 app.use(express.json());
 app.use(express.static("."));
